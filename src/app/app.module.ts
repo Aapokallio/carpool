c755e5106e627c6eb2219fb712ed54fcd9ba2bea
Aapo Kallio <aapo.kallio@gmail.com>
2023-01-03 17:28:44 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventComponent } from './event/event.component';
+import {RouterModule, RouterOutlet, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'event',component: EventComponent }
+  ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
